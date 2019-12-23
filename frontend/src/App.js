c94b00f3d2c01b2b020bea7bf696712e2f80083f
Facucode <34733879+Facucode@users.noreply.github.com>
2019-12-23 00:52:16 -0300

    Building CreateUser component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+//Brouwser router premite crear rutas, url para la app
+//Route sirve para especificar las rutas
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
+import Navigation from './components/Navigation'
+import NotesList from './components/NotesList'
+import CreateNote from './components/CreateNote'
+import CreateUser from './components/CreateUser'
 function App() {
   return (
-    <div>
-      hello world
+    <Router>
+      <Navigation/>
+      <div className="container p-4">
+      <Route path="/" exact component={NotesList} />
+      <Route path="/edit/:id" component={CreateNote} />
+      <Route path="/create" component={CreateNote} />
+      <Route path="/user" component={CreateUser} />
       </div>
+
+      </Router>
   );
 }
-
+// path="/"  cuando visite la ruta inicial de
+// la app que muestre el componente x
+// exact para que muestre solo esa ruta
 export default App;
---
+++ frontend/src/components/CreateUser.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+
+export default class CreateUser extends Component {
+    //ayuda a ejecutar funciones una vez que se monta el componente
+    //clg --> console.log()
+
+    state ={
+        users : [],
+        username: ""
+    }
+
+   async componentDidMount() {
+       const res = await axios.get('http://localhost:4000/api/users')
+       this.setState({users:res.data})
+    }
+    //con funcion flecha no es necesario escribir bind
+    onChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="card card-body">
+                        <h3>Create new user</h3>
+                        <form>
+                            <div className="form-group">
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    onChange={this.onChangeUsername}/>
+                            </div>
+                        </form>
+                    </div>
+                 </div>
+                <div className="col-md-8">
+                    <ul className="list-group">
+                        {
+                            this.state.users.map(user=>
+                                <li className="list-group-item list-group-item-action" key={user._id}>
+                                    {user.username}
+                                </li>)
+                        }
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
